98064f357295ad772041691848f7d76fb5593cf6
David Wolfand <david@unitedincome.com>
2019-01-16 15:51:46 -0500

    Updates

---
+++ precache-manifest.f27658e05b9bd393d27e7ba6c903e8ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c93de6093307b267cfe",
+    "url": "/static/js/main.2c93de60.chunk.js"
+  },
+  {
+    "revision": "eede3538b65417a8a09f",
+    "url": "/static/js/1.eede3538.chunk.js"
+  },
+  {
+    "revision": "2c93de6093307b267cfe",
+    "url": "/static/css/main.d53e6e8c.chunk.css"
+  },
+  {
+    "revision": "781cfcdd8713554f530c13d454f3ccfe",
+    "url": "/index.html"
+  }
+];
