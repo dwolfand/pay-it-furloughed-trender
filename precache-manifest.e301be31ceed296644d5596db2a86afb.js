c301c0c5787de2c9e5208964ef77390aabfe789c
David Wolfand <david@unitedincome.com>
2019-01-17 14:15:44 -0500

    Updates

--- precache-manifest.e301be31ceed296644d5596db2a86afb.js
+++ precache-manifest.e301be31ceed296644d5596db2a86afb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pay-it-furloughed-trender/static/js/runtime~main.58401384.js"
   },
   {
-    "revision": "90b0359fb591d2bd8bb2",
-    "url": "/pay-it-furloughed-trender/static/js/main.90b0359f.chunk.js"
+    "revision": "7a5c63c795720738417d",
+    "url": "/pay-it-furloughed-trender/static/js/main.7a5c63c7.chunk.js"
   },
   {
-    "revision": "5a81b3482d66335b66c8",
-    "url": "/pay-it-furloughed-trender/static/js/1.5a81b348.chunk.js"
+    "revision": "7bbd727480e5f191bfa6",
+    "url": "/pay-it-furloughed-trender/static/js/1.7bbd7274.chunk.js"
   },
   {
-    "revision": "90b0359fb591d2bd8bb2",
+    "revision": "7a5c63c795720738417d",
     "url": "/pay-it-furloughed-trender/static/css/main.d4bc625c.chunk.css"
   },
   {
-    "revision": "7721e59488d330461eef63aabbd2f28b",
+    "revision": "bd1d9aece78a59f50353598a1daaacf3",
     "url": "/pay-it-furloughed-trender/index.html"
   }
 ];
